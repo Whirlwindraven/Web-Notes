92c9d3d24566dcf9271f95fc863ad87279e86f09
Whirlwindraven <fadysherif1998@gmail.com>
2023-04-19 16:22:56 -0400

    created async function that updates the 'db.json' file whenever a note is added or deleted

--- routes/routes.js
+++ routes/routes.js
@@ -1,46 +1,52 @@
 const fileSystem = require('fs').promises;
 const filePath = require('path');
 
-// Initializing the module 
-module.exports = async application => {
-    //Reading and parsing the JSON file
-    let noteList;
+module.exports = async server => {
+  let noteArray;
+  try {
+    const fileData = await fileSystem.readFile('db/db.json', 'utf-8');
+    noteArray = JSON.parse(fileData);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  server.get('/api/notes', (request, response) => {
+    response.json(noteArray);
+  });
+
+  server.post('/api/notes', async (request, response) => {
+    const addedNote = request.body;
+    noteArray.push(addedNote);
+    await syncDb();
+    return response.status(201).json({ message: 'Note added successfully' });
+  });
+
+  server.get('/api/notes/:id', (request, response) => {
+    response.json(noteArray[request.params.id]);
+  });
+
+  server.delete('/api/notes/:id', async (request, response) => {
+    noteArray.splice(request.params.id, 1);
+    await syncDb();
+    return response.json({ message: 'Note deleted successfully' });
+  });
+
+  server.get('/notes', (request, response) => {
+    response.sendFile(filePath.join(__dirname, '../public/notes.html'));
+  });
+
+  server.get('*', (request, response) => {
+    response.sendFile(filePath.join(__dirname, '../public/index.html'));
+  });
+
+  async function syncDb() {
     try {
-      const fileData = await fileSystem.readFile('db/db.json', 'utf-8');
-      noteList = JSON.parse(fileData);
+      await fileSystem.writeFile('db/db.json', JSON.stringify(noteArray, null, 2));
+      console.log('Database updated successfully');
     } catch (error) {
       console.error(error);
-      process.exit(1);
+      throw error;
     }
-    app.get('/api/notes', (request, response) => {
-        response.json(noteList);
-      });
-      
-    app.post('/api/notes', async (request, response) => {
-        const addedNote = request.body;
-        noteList.push(addedNote);
-        await saveToDatabase();
-        return response.status(201).json({ message: 'Note added successfully' });
-      });
-      app.post('/api/notes', async (request, response) => {
-        const addedNote = request.body;
-        noteList.push(addedNote);
-        await saveToDatabase();
-        return response.status(201).json({ message: 'Note added successfully' });
-      }); 
-      app.get('/api/notes/:id', (request, response) => {
-        response.json(noteList[request.params.id]);
-      });
-      app.delete('/api/notes/:id', async (request, response) => {
-        noteList.splice(request.params.id, 1);
-        await saveToDatabase();
-        return response.json({ message: 'Note deleted successfully' });
-      });
-      app.get('/notes', (request, response) => {
-        response.sendFile(filePath.join(__dirname, '../public/notes.html'));
-      });
-    
-      app.get('*', (request, response) => {
-        response.sendFile(filePath.join(__dirname, '../public/index.html'));
-      });
-      
+  }
+};
