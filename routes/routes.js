fce66a5320fb7d08df2a52a88c9adf93b35a2257
Whirlwindraven <fadysherif1998@gmail.com>
2023-04-19 11:13:21 -0400

    Initiliazined module, created async function to read and parse JSON file, and GET/API/notes endpoint

--- routes/routes.js
+++ routes/routes.js
@@ -1,2 +1,18 @@
 const fileSystem = require('fs').promises;
 const filePath = require('path');
+
+// Initializing the module 
+module.exports = async application => {
+    //Reading and parsing the JSON file
+    let noteList;
+    try {
+      const fileData = await fileSystem.readFile('db/db.json', 'utf-8');
+      noteList = JSON.parse(fileData);
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+    application.get('/api/notes', (request, response) => {
+        response.json(noteList);
+      });
+      
